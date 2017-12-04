8324644813f7e1a2470c03385c5079deaf5b6df8
Ilya Tretyakov <it@it3xl.ru>
2017-12-05 00:21:43 +0300

    Input repo parameters from a file. Removing of the export command to prevent devOps from a mess. Checking of input repo parameters.

--- repo_settings/sample_repo.sh
+++ repo_settings/sample_repo.sh
@@ -10,19 +10,19 @@ project_folder=sample_repo
 
 
 # A conventional prefix for refs for repo 1.
-export prefix_1=client_co
-#export url_1=https://your-repo1-url.org/git/my_repo.git
-export url_1="$path_git_sync/sample_repos/remote_$prefix_1"
+prefix_1=client_co
+#url_1=https://your-repo1-url.org/git/my_repo.git
+url_1="$path_git_sync/sample_repos/remote_$prefix_1"
 
 
 # A conventional prefix for refs for repo 1.
-export prefix_2=vendor_co
-#export url_2=https://git.your-repo2-url.org/my_repo.git
-export url_2="$path_git_sync/sample_repos/remote_$prefix_2"
+prefix_2=vendor_co
+#url_2=https://git.your-repo2-url.org/my_repo.git
+url_2="$path_git_sync/sample_repos/remote_$prefix_2"
 
 
 # Some branch that must exist on both sides to allow any deletion of refs.
-export must_exist_branch=$prefix_1/production
+must_exist_branch=$prefix_1/production
 
 
 
